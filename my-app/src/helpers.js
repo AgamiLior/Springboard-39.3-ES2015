1c7e4e3fbcdd65260b21c7cceff71d5ad183b84c
Lior Agami <lioragami@liors-mbp.lan>
2022-04-24 13:04:50 -0400

    first

---
+++ my-app/src/helpers.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const choice = (items) => {
+	return items[Math.floor(items.length * Math.random())]
+}
+
+const remove = (items, item) => {
+	for (let i=0; i < items.length; i++) {
+		if (items[i] === item) {
+			items.splice(i,1);
+			return item
+		} 
+	}
+	return undefined
+}
+
+
+export {choice, remove};
---
+++ my-app/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {choice, remove} from "./helpers"
+import foods from './foods'
+
+let food = choice(foods);
+console.log(`I'd like one ${food}, please.`)
+console.log(`Here you go: ${food}`)
+console.log(`Delicious! May I have another?`)
+remove(foods, food)
+console.log(`I'm sorry, we're all out. We have ${foods} left`)
